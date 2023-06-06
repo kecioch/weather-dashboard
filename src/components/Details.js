9a1faa9b71536ccb972c97bb99a0b4d713f0e88a
Kevin Cioch <kevin.cioch@gmail.com>
2023-06-06 21:07:44 +0200

    added icons

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,22 +1,34 @@
+import {
+  BrightnessHigh,
+  Droplet,
+  Sunrise,
+  Sunset,
+  Umbrella,
+  Wind,
+} from "react-bootstrap-icons";
 import styles from "./Details.module.css";
 
 const Details = ({ data }) => {
   return (
     <div className={styles.container}>
       <p className={styles.rain}>
-        RG {`${data?.rain.value}${data?.rain.unit}`}
+        <Umbrella /> {`${data?.rain.value}${data?.rain.unit}`}
       </p>
       <p className={styles.humidity}>
-        LF {`${data?.humidity.value}${data?.humidity.unit}`}
+        <Droplet /> {`${data?.humidity.value}${data?.humidity.unit}`}
       </p>
       <p className={styles.uv}>
-        UV {`${data?.uv_index.value}${data?.uv_index.unit}`}
+        <BrightnessHigh /> {`${data?.uv_index.value}${data?.uv_index.unit}`}
       </p>
       <p className={styles.wind}>
-        WD {`${data?.windspeed.value}${data?.windspeed.unit}`}
+        <Wind /> {`${data?.windspeed.value}${data?.windspeed.unit}`}
       </p>
-      <p className={styles.sunrise}>SA {data?.sunrise}</p>
-      <p className={styles.sunset}>SU {data?.sunset}</p>
+      <p className={styles.sunrise}>
+        <Sunrise /> {data?.sunrise}
+      </p>
+      <p className={styles.sunset}>
+        <Sunset /> {data?.sunset}
+      </p>
     </div>
   );
 };
--- src/components/PreviewDay.js
+++ src/components/PreviewDay.js
@@ -1,18 +1,20 @@
 import styles from "./PreviewDay.module.css";
 import WeatherIcon from "./WeatherIcon";
+import { Umbrella } from "react-bootstrap-icons";
 
 const PreviewDay = ({ day }) => {
+  const date = day && new Date(day.date);
   return (
     <div className={styles.card}>
-      <p>
-        {day?.date.toLocaleString(navigator.language, { weekday: "long" })}
-      </p>
+      <p>{date?.toLocaleString(navigator.language, { weekday: "long" })}</p>
       <WeatherIcon />
       <p>
         {`${day?.temperature.max}${day?.temperature.unit}`} /{" "}
         {`${day?.temperature.min}${day?.temperature.unit}`}
       </p>
-      <p>RG {`${day?.rain.value}${day?.rain.unit}`}</p>
+      <p>
+        <Umbrella /> {`${day?.rain.value}${day?.rain.unit}`}
+      </p>
     </div>
   );
 };
